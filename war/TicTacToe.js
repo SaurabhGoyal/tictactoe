b1de19ca033dd55d69390847ada663e3093ffa11
Saurabh Goyal <saurabh.2561@gmail.com>
2015-01-14 17:19:54 +0530

    perfectly complete

--- war/TicTacToe.js
+++ war/TicTacToe.js
@@ -1,6 +1,6 @@
 var url = "gameresponse.jsp";
 $(function() {
-	$("#gametable").hide();
+	$("#gametable *").prop("disabled",true);
 	$("#firstyes").click(function(){
 		$.fn.firstQuery(1);
 	});
@@ -8,24 +8,18 @@ $(function() {
 		$.fn.firstQuery(0);
 	});
 	$(".gametablebuttons").click(function(){
-		//var sourceid = $(event.target).attr('id');
-		$(this).text("X");
+		$(this).text("X").prop("disabled", true);
 		var $ij = $(this).attr('id').substring(1);
-		//alert(ij);
+		$("#status").text("Thinking...");
 		$(this).secondQuery($ij);
 	});
 	$.fn.firstQuery = function(firstmove) {
-		$("#gametable").show();
+		$("#gametable *").prop("disabled",false);
 		$.get(url, {
 			qtype:'1',
 			first:firstmove
 		}, function(data) {
-			var $response = $('<div />').html(data);
-			var $status = $response.find('#status')
-			var $gametable = $response.find('#gametable');
-			$("#status").html($($status).html());
-			var ij = $($gametable).text();
-			$("#b"+ij).text("O");
+			$("mainbody").processData(data);
 		}, 'html')
 	}
 	$.fn.secondQuery = function(ijval) {
@@ -33,12 +27,18 @@ $(function() {
 			qtype:'2',
 			ij:ijval
 		}, function(data) {
-			var $response = $('<div />').html(data);
-			var $status = $response.find('#status')
-			var $gametable = $response.find('#gametable');
-			$("#status").html($($status).html());
-			var ij = $($gametable).text();
-			$("#b"+ij).text("O");
+			$("mainbody").processData(data);
 		}, 'html')
+	}
+	$.fn.processData = function(data) {
+		var $response = $('<div />').html(data);
+		var $status = $response.find('#status')
+		var $gametable = $response.find('#gametable');
+		var $statuscode = $response.find('#statuscode');
+		$("#status").html($($status).html());
+		var ij = $($gametable).text();
+		var sc = $($statuscode).text();
+		$("#b"+ij).text("O").prop("disabled", true);
+		if(sc!=3) $("#gametable *").prop("disabled",true);
 	}
 });
--- war/gameresponse.jsp
+++ war/gameresponse.jsp
@@ -7,12 +7,14 @@
 	String rType2 = "rtype2";
 	String splitter = ":||:";
 	MiniMaxGame game = null;
-	String statusResponse = "", gameTableResponse="";
+	String statusResponse = "", gameTableResponse="", statusCode="";
 	if (qtype == 1) {
 		game = new MiniMaxGame();
-		System.out.println("going to serialize");
+		//System.out.println("going to serialize");
+		session.invalidate();
+		session = request.getSession();
 		session.setAttribute("gameobject", game);
-		System.out.println("serialized");
+		//System.out.println("serialized");
 		int first = Integer.parseInt(request.getParameter("first"));
 		game.startGame(first);
 		if(first==0){
@@ -24,9 +26,10 @@
 		switch(status){
 			case 0: statusResponse = "You lost."; break;
 			case 1: statusResponse = "You won... I am not unbeatable apparently"; break;
-			case 2: statusResponse = "It's a draw. You know your shit."; break;
+			case 2: statusResponse = "It's a draw. You know your s**t."; break;
 			case 3: statusResponse = "Your turn ..."; break;
 		}
+		statusCode = status+"";
 	} else if (qtype == 2) {
 		game = (MiniMaxGame) session.getAttribute("gameobject");
 		String ij =  request.getParameter("ij");
@@ -39,12 +42,15 @@
 		switch(status){
 			case 0: statusResponse = "You lost."; break;
 			case 1: statusResponse = "You won... I am not unbeatable apparently"; break;
-			case 2: statusResponse = "It's a draw. You know your shit."; break;
+			case 2: statusResponse = "It's a draw. You know your s**t."; break;
 			case 3: statusResponse = "Your turn ..."; break;
 		}
+		if(status!=3) session.invalidate();
 		Pair lastMove = game.latestPair;
 		gameTableResponse = ""+(lastMove.i+1)+""+(lastMove.j+1)+"";
+		statusCode = status+"";
 	}
 %>
 <div id="status"><%= statusResponse %></div>
 <div id="gametable"><%=gameTableResponse %></div>
+<div id="statuscode"><%= statusCode %></div>
